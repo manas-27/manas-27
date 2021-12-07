8318939f0ec559c2209c7838530686d33b7d2602
Achyutmanas Tiwari <ammt501@gmail.com>
2021-12-07 15:22:17 +0530

    bigO of nested loops

---
+++ javadsa/bigOofNXN.java
@@ -0,0 +1,17 @@
+package javadsa;
+
+public class bigOofNXN {
+    static void multiArray() {
+        for (int i = 1; i <= 5; i++) { // O(n)
+            for (int j = 1; j <= 5; j++) { // O(n)
+                System.out.print(i + "" + j + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        multiArray();
+
+    }
+} // for nested loops O(n*n) = O(n^2)
