b02e96bf7403d69ce8a7c93e8f08ade96d2a5f24
Achyutmanas Tiwari <ammt501@gmail.com>
2022-07-13 20:37:54 +0530

    Web

---
+++ Web Development/Project/script.js
@@ -0,0 +1,31 @@
+let bar = document.getElementById("bar");
+let nav = document.getElementById("navbar");
+let close = document.getElementById("close");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+function login_btn() {
+  document.location.href = "login.html";
+}
+
+function login_btn_login_page() {
+  document.location.href = "index.html";
+}
+
+function register_btn_login_page() {
+  document.location.href = "index.html";
+}
+
+bannerHome = document.querySelector(".banner-2");
+bannerHome.addEventListener("click", () => {
+  document.location.href = "products.html";
+});
